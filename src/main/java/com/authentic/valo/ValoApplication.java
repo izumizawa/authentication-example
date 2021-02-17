6d28e9929b2a12461b37ef7aaca98918ba40b1aa
izumizawa <mariana.izumizawa@matera.com>
2021-02-17 17:45:47 -0300

    first commit

---
+++ src/main/java/com/authentic/valo/ValoApplication.java
@@ -0,0 +1,13 @@
+package com.authentic.valo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentic/valo/ValoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentic.valo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
