9809228ae2f308902ead9743ba3c8ab374ad412a
izumizawa <mariana.izumizawa@matera.com>
2021-02-19 14:13:13 -0300

    feat: create login screen

--- src/ui/src/Screens/Login/Login.tsx
+++ src/ui/src/Screens/Login/Login.tsx
@@ -1,11 +1,66 @@
 import React from "react";
-import { Button, Box } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  TextField,
+  Box,
+  Link,
+  Typography,
+} from "@material-ui/core";
+import useStyles from "./Style";
 
 const Login: React.FunctionComponent = () => {
+  const Styles = useStyles();
+
   return (
-    <Box component="span" m={1}>
-      <Button />
-    </Box>
+    <Container component="main" maxWidth="xs">
+      <Box className={Styles.formBox}>
+        <Typography className={Styles.title}>🌈 Welcome!</Typography>
+        <TextField
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          type="username"
+          variant="outlined"
+          margin="normal"
+          autoComplete="username"
+          className={Styles.textInput}
+        />
+        <TextField
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          type="password"
+          variant="outlined"
+          margin="normal"
+          autoComplete="password"
+          className={Styles.textInput}
+        />
+        <Typography className={Styles.forgotPassword}>
+          <Link href="#" className={Styles.link}>
+            Forgot you password?
+          </Link>
+        </Typography>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={Styles.submit}
+        >
+          Login
+        </Button>
+        <Typography>
+          Need an account?
+          <Link href="#" className={Styles.link}>
+            {" "}
+            Sign up
+          </Link>
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
---
+++ src/ui/src/Screens/Login/Style.ts
@@ -0,0 +1,44 @@
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  formBox: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#FA4454",
+    borderColor: "#FA4454",
+    "&:hover": {
+      backgroundColor: "#DC3D4B",
+      borderColor: "#DC3D4B",
+      boxShadow: "none",
+    },
+  },
+  link: {
+    color: "#FA4454",
+  },
+  forgotPassword: {
+    alignSelf: "flex-start",
+    fontSize: 14,
+  },
+  textInput: {
+    // border: "#FF8189",
+    "& label.Mui-focused": {
+      color: "#364966",
+    },
+    "& .MuiOutlinedInput-root": {
+      "&.Mui-focused fieldset": {
+        borderColor: "#364966",
+      },
+    },
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: theme.spacing(5),
+  },
+}));
+// #FF8189
+export default useStyles;
